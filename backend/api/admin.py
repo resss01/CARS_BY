53e37977867870b29138a7b1ab003142eec8a1f5
Suvhanov <resulsuvhanov@gmail.com>
2025-08-24 20:05:51 +0300

    second commit

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,11 +3,11 @@ from .models import Car_list
 
 @admin.register(Car_list)
 class Car_list_admin(admin.ModelAdmin):
-    list_display = ('id', 'brand', 'model', 'engine', 'price_usd', 'price_byn', 'description', 'image')
+    list_display = ('id', 'brand', 'model', 'engine', 'price_usd', 'price_byn', 'image')
     list_display_links = ('id', 'brand', 'model')
     search_fields = ('brand', 'model', 'description')
     list_filter = ('brand', 'model', 'engine', 'price_usd', 'price_byn')
-    list_editable = ('price_usd', 'price_byn', 'description')
+    list_editable = ('price_usd', 'price_byn')
     list_per_page = 10
     list_max_show_all = 100
 
